bb39e51e9b86412b7ad6c77c3cae0ba4fa4ff0c0
zhoushiya <zhoushiya@126.com>
2020-01-01 20:50:11 +0800

    完成平台的制作与销毁

---
+++ Assets/Scripts/Fan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+    private Animation _animation;
+
+    private void Awake()
+    {
+        _animation = GetComponent<Animation>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _animation.Play("Run");
+        }
+    }
+}
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private Vector3 _movement;
+
+    private float _topLineY;
+
+    public float Speed;
+
+    private void Awake()
+    {
+        _movement.y = Speed;
+        _topLineY = GameObject.Find("TopLine").transform.position.y;
+    }
+
+    void Update()
+    {
+        MoveUp();
+    }
+
+    void MoveUp()
+    {
+        transform.position += _movement * Time.deltaTime;
+        if (transform.position.y >= _topLineY)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
